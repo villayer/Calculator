4ff6c90b36f416719cf71e2ab0b57b3d8d8536d4
villayer <rainbowcanbeblack@gmail.com>
2021-06-21 12:34:01 +0100

    added repo files

---
+++ ComputePrograme.c
@@ -0,0 +1,11 @@
+/* this fucking program is property of me vi aka villayer
+mail me or whatever i dont care pute!
+also this GPL(vi)3 
+*/
+
+#include <gtk/gtk.h>
+#include <string.h>
+
+int main(){
+    printf("how about you?");
+}
